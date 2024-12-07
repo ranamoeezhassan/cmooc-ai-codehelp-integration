7e9b4a9ddf5d1451559755e74db4b4e88e92738b
ranamoeezhassan <rmhass26@colby.edu>
2024-12-07 00:36:13 -0500

    Fixed a small problem in database tracking

---
+++ src/codehelp/migrations/update_dartmouth_models.sql
@@ -0,0 +1,22 @@
+-- src/gened/migrations/[timestamp]_update_dartmouth_models.sql
+
+PRAGMA foreign_keys = OFF;
+
+BEGIN;
+
+-- Update models table with Dartmouth models
+UPDATE models SET 
+    name='Dartmouth CodeLlama 13B',
+    shortname='CodeLlama-13B',
+    model='codellama-13b-instruct-hf'
+WHERE model='gpt-4o';
+
+UPDATE models SET
+    name='Dartmouth Llama 3',
+    shortname='Llama-3',
+    model='llama-3-1-8b-instruct'
+WHERE model='gpt-4o-mini';
+
+COMMIT;
+
+PRAGMA foreign_keys = ON;
